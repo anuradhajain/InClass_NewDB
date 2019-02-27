f48b322d10b3771f872b2cde31283feea49e1db5
anuradhajain <46470005+anuradhajain@users.noreply.github.com>
2019-02-26 21:31:14 -0500

    Add files via upload

---
+++ Create dbo.PSales.sql
@@ -0,0 +1,31 @@
+﻿USE [Sales]
+GO
+
+/****** Object: Table [dbo].[PSales] Script Date: 2/26/2019 9:21:23 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+/* Create Table code copied from View designer*/
+CREATE TABLE [dbo].[PSales]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [orderDate] DATE NULL, 
+    [Quantity] INT NULL, 
+    [shipDate] DATE NULL, 
+    [ProductName] NCHAR(10) NULL
+)
+
+/* Original Create Table
+CREATE TABLE [dbo].[PSales] (
+    [Id]          INT        NOT NULL,
+    [orderDate]   DATE       NULL,
+    [Quantity]    INT        NULL,
+    [shipDate]    DATE       NULL,
+    [ProductName] NCHAR (10) NULL
+);
+*/
+
+
---
+++ Create dbo.Product.sql
@@ -0,0 +1,34 @@
+﻿USE [Sales]
+GO
+
+/****** Object: Table [dbo].[Product] Script Date: 2/26/2019 9:23:35 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+/* Create Table code copied from View designer*/
+CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProductName] NVARCHAR(50) NOT NULL, 
+    [Category] NVARCHAR(50) NULL, 
+    [ManDate] DATE NULL, 
+    [Batch] NVARCHAR(50) NULL, 
+    [ExpirationDate] DATE NULL, 
+    [Specification] NVARCHAR(100) NULL
+)
+
+/* Original Create Table
+CREATE TABLE [dbo].[Product] (
+    [Id]             INT            NOT NULL,
+    [ProductName]    NVARCHAR (50)  NOT NULL,
+    [Category]       NVARCHAR (50)  NULL,
+    [ManDate]        DATE           NULL,
+    [Batch]          NVARCHAR (50)  NULL,
+    [ExpirationDate] DATE           NULL,
+    [Specification]  NVARCHAR (100) NULL
+);
+*/
+
